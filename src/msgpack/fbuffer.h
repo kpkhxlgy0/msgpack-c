de29a0c56a9fc0b6a7186dbbbe8c9329728adb18
XiaoLongHan <kpkhxlgy0@163.com>
2014-09-11 16:34:44 +0800

    x

--- src/msgpack/fbuffer.h
+++ src/msgpack/fbuffer.h
@@ -1,7 +1,7 @@
 /*
- * MessagePack unpacking routine template
+ * MessagePack for C FILE* buffer adaptor
  *
- * Copyright (C) 2008-2010 FURUHASHI Sadayuki
+ * Copyright (C) 2013 Vladimir Volodko
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -15,12 +15,33 @@
  *    See the License for the specific language governing permissions and
  *    limitations under the License.
  */
-#ifndef MSGPACK_PACK_DEFINE_H__
-#define MSGPACK_PACK_DEFINE_H__
+#ifndef MSGPACK_FBUFFER_H__
+#define MSGPACK_FBUFFER_H__
 
-#include "msgpack/sysdep.h"
-#include <limits.h>
-#include <string.h>
+#include <stdio.h>
 
-#endif /* msgpack/pack_define.h */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+ * @defgroup msgpack_fbuffer FILE* buffer
+ * @ingroup msgpack_buffer
+ * @{
+ */
+
+static inline int msgpack_fbuffer_write(void* data, const char* buf, unsigned int len)
+{
+	return (1 == fwrite(buf, len, 1, (FILE *)data)) ? 0 : -1;
+}
+
+/** @} */
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* msgpack/fbuffer.h */
 
